5877f1700e42e7bebcfb0398239a527ed55911c8
senior9904 <radostin.tasev22@gmail.com>
2026-01-18 10:45:06 +0200

    Create person.js

---
+++ person.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+
+  haveBirthday() {
+    this.age += 1;
+  }
+}
